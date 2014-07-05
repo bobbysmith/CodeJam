603191f6d97e9700fb30f46833f34c3326013e66
Bobby Smith <bobbysmith24@gmail.com>
2014-07-05 11:26:54 -0500

    songs controller methods, view form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,48 @@
 class SongsController < ApplicationController
+  before_filter :authenticate
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = current_user.songs.new
+  end
+
+  def create
+    song = current_user.songs.new(song_params)
+    song.save
+    redirect_to root_path
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+
+    if params[:song][:url].blank?
+      flash[:error] = "Please enter a URL"
+      # params[:song].delete(:song)
+    end
+
+    @song.update(song_params)
+    redirect_to user_path[:id]
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to root_path
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:url, :user_id)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+if $0 == 'irb'
+  require 'hirb'
+  Hirb.enable
+end
 # Initialize the Rails application.
 Rails.application.initialize!
+
